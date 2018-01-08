aefb8eb105833772874e8df51bd131b4c5475e9f
geun <limgeunyoung90@gmail.com>
2018-01-08 10:56:46 +0900

    css sass로 변경

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var ghPages = require('gulp-gh-pages');
 
 gulp.task('sass', function(){
     return gulp.src('./sass/index.scss')
@@ -9,6 +10,11 @@ gulp.task('sass', function(){
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./css'));
 });
+/*
+gulp.task('deploy', function() {
+    return gulp.src('')
+    .pipe(ghPages());
+});*/
 
 gulp.task('default', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
