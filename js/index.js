d4e7d836a39b87d1c3bee4af946093bb1b538bb6
geun <limgeunyoung90@gmail.com>
2018-01-10 14:24:52 +0900

    product클릭시 사이드메뉴 나옴 (트윈맥스이용) 구현

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 (function(){
 
+
+
+    // product 클릭시
+    var productLink = $("[data-ui=productLink]");
     var side_menu = $("[data-ui=side_menu]");
+    var sideMenu_back = $('[data-ui=sidemenu_back]');
+    
+    productLink.click(function(){
+        TweenMax.to(side_menu, .3, {left:0});
+    });
+    
+    sideMenu_back.click(function(){
+        TweenMax.to(side_menu, .3, {left:-500});
+    });
+    
+    
+    
+    //사이드 메뉴 링크 마우스 오버시 밑줄
     var side_menu_link = $("[data-ui=side_menu] a");
 
     side_menu_link.mouseover(function() {
@@ -11,4 +28,6 @@
        
     });
 
-}());
+
+
+}())
