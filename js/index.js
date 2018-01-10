3dc163719b6a77611730d2e7778b06cfa6ec1e4f
geun <limgeunyoung90@gmail.com>
2018-01-10 18:54:52 +0900

    사이드메뉴 메뉴 마우스오버시 기능 구현중

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 (function(){
 
-
+/*
 
     // product 클릭시
     var productLink = $("[data-ui=productLink]");
@@ -15,7 +15,19 @@
         TweenMax.to(side_menu, .4, {left:-500});
     });
     
-    
+*/
+    var side_menu = $("[data-ui=side_nav] a");
+    var sideMenu_detail = $("[data-ui=sideMenu_detail]");
+    side_menu.mouseover(function() {
+        sideMenu_detail.show();
+        TweenMax.to(sideMenu_detail, 1, {width:"100%"})
+    })
+    .mouseout(function() { 
+        //sideMenu_detail.hide();
+       TweenMax.to(sideMenu_detail, 1, {width:"0%"})
+       
+    });
+
     // navi a 마우스 오버시 밑줄 생성 
     var navi_link =$('nav a');
     
