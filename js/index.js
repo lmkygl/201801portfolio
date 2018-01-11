0e50b839885bdf81b6cc83331becd0e51a9a2ec6
geun <limgeunyoung90@gmail.com>
2018-01-11 14:36:34 +0900

    product click시 사이드메뉴 나타남 구현

--- js/index.js
+++ js/index.js
@@ -1,50 +1,20 @@
 (function(){
 
-
-    // product 클릭시
-    var productLink = $("[data-ui=productLink]");
-    var side_menu = $('[data-ui="side_menu"]');
-    var sideMenu_back = $('[data-ui=sidemenu_back]');
-    
-    productLink.click(function(){
-        TweenMax.to(side_menu, .4, {left:0, onComplete: function() {
-           // console.log(11)
-        }});
-    });
-
-    sideMenu_back.click(function(){
-        TweenMax.to(side_menu, .4, {left:"-1000px"});
-    });
-    
-
-  //  var side_menu = $("[data-ui=side_nav] a");
-    var sideMenu_detail = $("[data-ui=sideMenu_detail]");
-    side_menu.mouseover(function() {
-        sideMenu_detail.show();
-        TweenMax.to(sideMenu_detail, 1, {width:"100%"})
-    })
-    .mouseout(function() { 
-        //sideMenu_detail.hide();
-       TweenMax.to(sideMenu_detail, 1, {})
-       
-    });
-
-    // navi a 마우스 오버시 밑줄 생성 
-   
-    var navi_Link_Event = $('[data-ui="naviLink"]');
+    // navi a 밑줄 효과
+    var navi_Link_Event = $('[data-ui="navi_Link"]');
 
     function navi_Effect(e){
         var naviEvnet_target =$(e.target);
         var naviEvnet_type = e.type;
         var naviEvent_line = naviEvnet_target.find('.underline');
-        var naviEvent_line_width = naviEvent_line.width();
+       // var naviEvent_line_width = naviEvent_line.width();
         
         // console.log(naviEvnet_target);
         // console.log(naviEvnet_type);
         // console.log(naviEvent_line_width);
        
         if( naviEvnet_type === 'mouseenter' ){
-           naviEvent_line.animate({width:"100%", backgroundColor:'#0D364C'}, .3 );
+           naviEvent_line.animate({width:"100%"}, .3 );
         }
        else if(naviEvnet_type === 'mouseleave' ){
            naviEvent_line.animate({width:0}, .3 ); 
@@ -52,6 +22,53 @@
     
     }
 
+   // product 클릭시 
+    var product_Link = $('[data-ui-side="product_Link"]');
+    var side_menu = $('[data-ui-side="side_menu"]');
+    var side_menu_close = $('[data-ui="sidemenu_close"]');
+    
+    function productClick_Effect(e){
+        TweenMax.to(side_menu, .4, {left:0});
+    }
+
+
     navi_Link_Event.on('mouseenter mouseleave', navi_Effect);
+    product_Link.on('click', productClick_Effect);
+
+
+
+
+     // product 클릭시
+    //  var productLink = $('[data-ui-side="productLink"]');
+    //  var side_menu = $('[data-ui="side_menu"]');
+    //  var sideMenu_back = $('[data-ui=sidemenu_back]');
+     
+    //  productLink.click(function(){
+    //      TweenMax.to(side_menu, .4, {left:0, onComplete: function() {
+    //         // console.log(11)
+    //      }});
+    //  });
+ 
+    //  sideMenu_back.click(function(){
+    //      TweenMax.to(side_menu, .4, {left:"-1000px"});
+    //  });
+     
+
+   
+
+//   //  var side_menu = $("[data-ui=side_nav] a");
+//     var sideMenu_detail = $("[data-ui=sideMenu_detail]");
+//     side_menu.mouseover(function() {
+//         sideMenu_detail.show();
+//         TweenMax.to(sideMenu_detail, 1, {width:"100%"})
+//     })
+//     .mouseout(function() { 
+//         //sideMenu_detail.hide();
+//        TweenMax.to(sideMenu_detail, 1, {})
+       
+//     });
+
+    // navi a 마우스 오버시 밑줄 생성 
+   
 
 }());
