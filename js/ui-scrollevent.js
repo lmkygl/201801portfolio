b1ea750bb7a64c9fa57a48189746a27a7a58e2ae
geun <limgeunyoung90@gmail.com>
2018-01-18 17:56:49 +0900

    함수명 변경

--- js/ui-scrollevent.js
+++ js/ui-scrollevent.js
@@ -20,7 +20,7 @@
     
     var $ui_motion = $('[data-ui-animate]');
     
-    function setMotion() {
+    function common_Animation() {
         TweenMax.to($ui_motion, 1.5 , {
             opacity: 1,
         });
@@ -38,7 +38,7 @@
     var $section1_center_explanation = $('[data-ui-animate="section1_center_explanation"]');
     var $section1_center_price = $('[data-ui-animate="section1_center_price"]');
     
-    function section1_setAnimate(){
+    function section1_setAnimation(){
         TweenMax.set($section1_left, {
             y: 400,
             opacity: 0
@@ -71,9 +71,9 @@
         });
     }
 
-    function section1_area(e){
-        section1_setAnimate();
-        setMotion();
+    function section1_playAnimation(e){
+        section1_setAnimation();
+        common_Animation();
     }
     
     var section2 = $('.section2');
@@ -84,7 +84,7 @@
     var $section2_center_explanation = $('[data-ui-animate="section2_center_explanation"]');
     var $section2_center_price = $('[data-ui-animate="section2_center_price"]');
     
-    function section2_setAnimate(){
+    function section2_setAnimation(){
         
         TweenMax.set($section2_left, {
             y: 250,
@@ -118,9 +118,9 @@
 
     }
 
-    function section2_area(e){
-        section2_setAnimate();
-        setMotion();
+    function section2_playAnimation(e){
+        section2_setAnimation();
+        common_Animation();
     }
 
 
@@ -132,7 +132,7 @@
     var $section3_center_explanation = $('[data-ui-animate="section3_center_explanation"]');
     var $section3_center_price = $('[data-ui-animate="section3_center_price"]');
 
-    function section3_setAnimate(){
+    function section3_setAnimation(){
 
         TweenMax.set($section3_left, {
             y: 500,
@@ -165,9 +165,9 @@
         });
     }
 
-    function section3_area(e){
-        section3_setAnimate();
-        setMotion();
+    function section3_playAnimation(e){
+        section3_setAnimation();
+        common_Animation();
     }
     
     var section4 = $('.section4');
@@ -178,7 +178,7 @@
     var $section4_center_explanation = $('[data-ui-animate="section4_center_explanation"]');
     var $section4_center_price = $('[data-ui-animate="section4_center_price"]');
 
-    function section4_setAnimate(){
+    function section4_setAnimation(){
 
         TweenMax.set($section4_left, {
             y: 300,
@@ -210,13 +210,13 @@
             opacity: 0
         });
     }
-    function section4_area(e){
-        section4_setAnimate();
-        setMotion();
+    function section4_playAnimation(e){
+        section4_setAnimation();
+        common_Animation();
     }
 
-    section1.on('click', section1_area);
-    section2.on('click', section2_area);
-    section3.on('click', section3_area);
-    section4.on('click', section4_area);
+    section1.on('click', section1_playAnimation);
+    section2.on('click', section2_playAnimation);
+    section3.on('click', section3_playAnimation);
+    section4.on('click', section4_playAnimation);
 }());
