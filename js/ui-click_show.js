20f279110133d3b68ec4fb827e5738b4e9e1e432
geun <limgeunyoung90@gmail.com>
2018-01-24 11:19:39 +0900

    ontv 버튼 클릭시 협찬상품 보여줌 , 토글버튼 작업중

--- js/ui-click_show.js
+++ js/ui-click_show.js
@@ -1,16 +1,35 @@
 (function(){
-    var $product_btn = $('[data-ui="sponsorship_show"]').find('button');
-    var $sponsorship_product_area = $('[data-ui="sponsorship_product_area"]');
+    var button = $('.list_Box').find('button');
+    var product = $('[data-ui="sponsorship_product_area"]');
 
-    function product_click_effect(e){
-        $sponsorship_product_area.show();
-        TweenMax.to($sponsorship_product_area, 0.7, {
-            opacity:1,
-            top:0
-        });
-        
+    function clickhandle(e){
+        var target = $(e.target);
+        var list = $(e.target).parents('.list_Box');
+        var $btn = $(e.target).parents('[data-ui="sponsorship_show"]').siblings('ul');
+        var toggled = false;
+
+        if (toggled) {
+            toggled = false;
+            console.log(toggled);
+            list.animate({"height":"550px"});
+            TweenMax.to($btn, 0.3, {
+                opacity:1,
+            });
+            target.text("▲");
+            return false;
+        }
+        if(!toggled){
+            toggled = true;
+            console.log(toggled);
+            list.animate({"height":"300px"});
+            TweenMax.to($btn, 0.3, {
+                opacity:0,
+            });
+            target.text("▼");
+            return false;
+        }
+
     }
-
-    $product_btn.on('click', product_click_effect);
-
+    
+    button.on('click', clickhandle);
 }());
