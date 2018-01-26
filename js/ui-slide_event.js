db1d50e25da559741a759e5edf7d14cfe9f733f8
geun <limgeunyoung90@gmail.com>
2018-01-26 17:57:16 +0900

    frdsflkjdsf

--- js/ui-slide_event.js
+++ js/ui-slide_event.js
@@ -4,6 +4,10 @@
     var animating = false;
     var animation_speed = 500;
 
+    var $ul = $('[data-ui="inner_frame"]');
+    currentIndex = 0;
+    
+
     function next_slide_effect(e){
         var $target = $(e.target);
         var $parents = $target.parents('[data-ui="slider"]');
@@ -17,7 +21,13 @@
         }, animation_speed, 'swing', function () {
             $scene.eq(0).insertAfter($scene.eq(-1));
             $inner_frame.css('left', 0);
+            currentIndex +=1;
+            if( currentIndex >= currentIndex.max  ){
+                currentIndex = 0;
+            }
+            console.log(currentIndex);
             animating = false;
+            
         });
     }
 
@@ -34,32 +44,38 @@
         $inner_frame.animate({
             left: 0
         }, animation_speed, 'swing', function(){
+            currentIndex -=1;
+            if( currentIndex <= -5 ){
+                currentIndex = 0;
+            }
+            console.log(currentIndex);
             animating = false;
+
         });
     }
 
-    function indcator_slide_effect(e){
-        var $target = $(e.target);
-        var $parents = $target.parents('[data-ui="slider"]');
-        var $indcator = $parents.find('.slide_indicator_btn');
-        var $indcator_index = $target.index();
-        var $out_width = $parents.find('[data-ui="out_frame"]').width();
-        var $inner_frame = $parents.find('[data-ui="inner_frame"]');
+    // function indcator_slide_effect(e){
+    //     var $target = $(e.target);
+    //     var $parents = $target.parents('[data-ui="slider"]');
+    //     var $indcator = $parents.find('.slide_indicator_btn');
+    //     var $indcator_index = $target.index();
+    //     var $out_width = $parents.find('[data-ui="out_frame"]').width();
+    //     var $inner_frame = $parents.find('[data-ui="inner_frame"]');
        
-        $indcator.removeClass('indicator_select');
-        $target.addClass('indicator_select');
+    //     $indcator.removeClass('indicator_select');
+    //     $target.addClass('indicator_select');
 
-        for(var i = 0; i < $out_width; i++){
-            if( $indcator_index == i ){
-                $inner_frame.animate({
-                    "left":"-"+ $out_width*$indcator_index +"px"
-                },
-                animation_speed, 'swing', function(){
-                    animating = false;
-                });
-            }
-        }
-    }
+    //     for(var i = 0; i < $out_width; i++){
+    //         if( $indcator_index == i ){
+    //             $inner_frame.animate({
+    //                 "left":"-"+ $out_width*$indcator_index +"px"
+    //             },
+    //             animation_speed, 'swing', function(){
+    //                 animating = false;
+    //             });
+    //         }
+    //     }
+    // }
 
     function slide_effect(e) {
       var $target = $(e.target);
@@ -75,12 +91,12 @@
             prev_slide_effect(e);
           }
       }
-      else if ($target.hasClass('slide_indicator_btn')) {
-          if (!animating) {
-              animating = true;
-              indcator_slide_effect(e);
-          }
-      }
+    //   else if ($target.hasClass('slide_indicator_btn')) {
+    //       if (!animating) {
+    //           animating = true;
+    //           indcator_slide_effect(e);
+    //       }
+    //   }
     }
 
     $btn.on('click', slide_effect);
