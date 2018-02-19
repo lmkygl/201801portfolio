66ae08cdf138e2dd6f1df5e175be427ed333833c
geunyoung <limgeunyoung90@gmail.com>
2018-02-19 12:53:26 +0900

    setinterval 연결

--- js/ui-indicator_slide.js
+++ js/ui-indicator_slide.js
@@ -14,23 +14,20 @@
 
     function slide_show(e) {
         // indecator index 
-        var target = $(e.target);
-        var target_index = target.index();
+        var $target = $(e.target);
+        var $target_index = $target.index();
         
         $indicator_btn.removeClass('select_indicator');
         $(this).addClass('select_indicator');
     
-        indecator_animating(target_index);
+        indecator_animating($target_index);
     }    
 
     function indecator_animating(index) {
         // slide animate 
-        
-
         var $side_out_frame = $('[data-ui="side_slide_out_frame"]');
         var $side_out_frame_width = $side_out_frame.width();
         var $side_inner_frame = $('[data-ui="side_slide_inner_frame"]');
-        var item = $side_out_frame.find('.side_item');
         
         $side_inner_frame.animate({
             marginLeft: -$side_out_frame_width * index
