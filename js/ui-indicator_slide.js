f4a22201d5b8f49339979a63e983a0738e3b8083
geunyoung <limgeunyoung90@gmail.com>
2018-02-19 16:58:53 +0900

    setinterval 작업중

--- js/ui-indicator_slide.js
+++ js/ui-indicator_slide.js
@@ -4,38 +4,36 @@
     var slide_start;
     var $indicator = $('.indicator');
     var $indicator_btn = $indicator.find('span');
-    var speed = 400;
+    var speed = 100;
     var animating = false;
-
-    function start_slide() {
-        slide_start = setInterval(slide_show, speed);
-    };
 
     function slide_show(e) {
         // indecator index 
         var $target = $(e.target);
         var $target_index = $target.index();
-        
+
         $indicator_btn.removeClass('select_indicator');
         $(this).addClass('select_indicator');
-    
+
         indecator_animating($target_index);
-        $target_index++;
-        if ($target_index >= $target_index.length) {
-            $target_index = 0;
-        }
-    }    
+        
+    }
 
     function indecator_animating(index) {
         // slide animate 
         var $side_out_frame = $('[data-ui="side_slide_out_frame"]');
         var $side_out_frame_width = $side_out_frame.width();
         var $side_inner_frame = $('[data-ui="side_slide_inner_frame"]');
-        
+
         $side_inner_frame.animate({
             marginLeft: -$side_out_frame_width * index
         }, speed, 'swing');
     }
+
+    function start_slide() {
+        slide_start = setInterval(indecator_animating, speed);
+    };
+    
     start_slide();
     $indicator_btn.on('click', slide_show);
 }());
