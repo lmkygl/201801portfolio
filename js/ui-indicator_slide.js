bb6725aa7360ecb9ca46a27f8f6e80df385c466e
geunyoung <limgeunyoung90@gmail.com>
2018-02-20 15:25:50 +0900

    setinterval 작업중

--- js/ui-indicator_slide.js
+++ js/ui-indicator_slide.js
@@ -16,7 +16,12 @@
         $(this).addClass('select_indicator');
 
         indecator_animating($target_index);
-        
+        clearInterval(start_slide);
+
+        $target_index++;
+        if ($target_index >= 3) {
+            $target_index = 0;
+        }
     }
 
     function indecator_animating(index) {
@@ -33,7 +38,7 @@
     function start_slide() {
         slide_start = setInterval(indecator_animating, speed);
     };
-    
+
     start_slide();
     $indicator_btn.on('click', slide_show);
 }());
