5287d8470a4d4331d5b95c1ed5f0cc14a5161ce7
geunyoung <limgeunyoung90@gmail.com>
2018-02-27 18:51:29 +0900

    filter 1차 코드

--- js/ui-list_filter.js
+++ js/ui-list_filter.js
@@ -1,4 +1,16 @@
 // list filter 
 (function(){
-    
+    var $search_text = $('[data-ui="search_text"]');
+    var $search_btn = $('[data-ui="search_btn"]');
+    function list_search(e){
+        var $search_val = $(this).val();
+        var $list = $('.store_info').find('li');
+        if ($search_val == '') {
+            $list.show();
+        }else {
+            $list.hide();
+            $('.store_info li:contains("'+ $search_val +'")').show();
+        }
+    }
+    $search_text.on('keyup', list_search); 
 }());
